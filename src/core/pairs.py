7b32e730119a02d4ba8345e1ddec07475f5f7979
Tony Su <peitaosu@163.com>
2020-01-12 22:03:25 +0800

    fix run issue

--- src/core/pairs.py
+++ src/core/pairs.py
@@ -17,7 +17,7 @@ from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Input, Lambda, Flatten
 from keras.optimizers import RMSprop
 from keras import backend as K
-from sklearn.neighbors import NearestNeighbors, LSHForest
+from sklearn.neighbors import NearestNeighbors
 from keras.layers import Conv2D, MaxPooling2D
 from keras.callbacks import LearningRateScheduler
 
