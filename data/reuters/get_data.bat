23e6ba2efe140f074dde315341c6104a74101c33
Tony Su <peitaosu@163.com>
2020-01-15 22:15:55 +0800

    add windows script

---
+++ data/reuters/get_data.bat
@@ -0,0 +1,12 @@
+wget http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a12-token-files/lyrl2004_tokens_test_pt0.dat.gz
+"C:\\Program Files\7-Zip\7z.exe" x lyrl2004_tokens_test_pt0.dat.gz
+wget http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a12-token-files/lyrl2004_tokens_test_pt1.dat.gz
+"C:\\Program Files\7-Zip\7z.exe" x lyrl2004_tokens_test_pt1.dat.gz
+wget http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a12-token-files/lyrl2004_tokens_test_pt2.dat.gz
+"C:\\Program Files\7-Zip\7z.exe" x lyrl2004_tokens_test_pt2.dat.gz
+wget http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a12-token-files/lyrl2004_tokens_test_pt3.dat.gz
+"C:\\Program Files\7-Zip\7z.exe" x lyrl2004_tokens_test_pt3.dat.gz
+wget http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a12-token-files/lyrl2004_tokens_train.dat.gz
+"C:\\Program Files\7-Zip\7z.exe" x lyrl2004_tokens_train.dat.gz
+wget http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a08-topic-qrels/rcv1-v2.topics.qrels.gz
+"C:\\Program Files\7-Zip\7z.exe" x rcv1-v2.topics.qrels.gz
